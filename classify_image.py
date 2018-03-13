dd6c5f074b02116c7a1d5e0c51cd24041d60ab6d
Malygina, Maria <Maria.Malygina@emc.com>
2018-03-13 18:23:36 +0300

    [initial commit]

---
+++ classify_image.py
@@ -0,0 +1,27 @@
+import numpy as np
+from PIL import Image
+
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+
+model = VGG16(weights='imagenet', include_top=True)
+
+layers = dict([(layer.name, layer.output) for layer in model.layers])
+layers
+
+model.count_params()
+
+image_path = 'images/elephant.png'
+image = Image.open(image_path)
+image = image.resize((224, 224))
+image.show()
+
+# Convert it into an array
+x = np.asarray(image, dtype='float32')
+# Convert it into a list of arrays
+x = np.expand_dims(x, axis=0)
+# Pre-process the input to match the training data
+x = preprocess_input(x)
+
+preds = model.predict(x)
+print('Predicted:', decode_predictions(preds, top=3)[0])
